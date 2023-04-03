3bb3646acdd24288c2fa13f705c2b7276b46f68a
Mark Reid <mark@reid.name>
2023-04-03 12:22:33 +1000

    Use ii.i2m.* style functions

--- README.md
+++ README.md
@@ -4,25 +4,60 @@ Simple crow library to send commands to the i2c2midi module.
 ## Basic Usage
 
 From `druid` load and run the library with `r i2c2midi.lua`. Once loaded, you can call i2c2midi
-ops via `i2m(op_name, arg1, arg2, ...)`.
+ops via `ii.i2mi.{op_name}(arg1, arg2, ...)`.
 
-For example,
+The available commands can be viewed using `ii.i2m.help()`:
 ```lua
--- Set all channels time to 1 second
-i2m("time", 0, 1000)
-
--- Define chord 1 to be minor triad using reverse binary
-i2m("c_b", 1, "10010001")
-
--- Set the strum interval on chord 1 to 30ms
-i2m("c_str", 1, 30)
-
--- Play chord 1 on channel 0, at note 60, with velocity 100
-i2m("chord", 0, 1, 60, 100) 
+> ii.i2m.help()
+panic()  -- Send MIDI off on all channels
+time(channel, time_ms)  -- Set the time (ms) before note off
+shift(channel, semitones)  -- Set note shift
+note_reps(channel, repetitions)  -- Set note repetition
+note_ratchets(channel, ratchets)  -- Set note ratcheting
+note_on(channel, note, velocity)  -- Send note on
+note_off(channel, note)  -- Send note off
+play_note(channel, note, velocity, duration_ms)  -- Send note on with duration
+play_chord(channel, chord, root, velocity)  -- Play with given root
+chord_add(chord, note)  -- Add a note
+chord_remove(chord, note)  -- Remove a note
+chord_delete(chord, index)  -- Delete indexed note
+chord_set(chord, index, note)  -- Set indexed note
+chord_binary(chord, rev_binary)  -- Define chord using reverse binary
+chord_clear(chord)  -- Clear all notes
+chord_length(chord, length)  -- Set length (# notes)
+chord_scale(chord, scale_chord)  -- Set scale of 1st chord to scale given by 2nd
+chord_reversal(chord, reversed)  -- Set reversal
+chord_rotation(chord, steps)  -- Set rotation
+chord_transpose(chord, semitones)  -- Set transposition
+chord_distort(chord, width, anchor)  -- Set distortion
+chord_reflect(chord, width, anchor)  -- Set reflection
+chord_inversion(chord, inversion)  -- Set inversion
+chord_strum(chord, time_ms)  -- Set duration (ms) between notes
+chord_vel_curve(chord, type, start_percent, end_percent)  -- Set velocity curve
+chord_time_curve(chord, type, start_percent, end_percent)  -- Set time curve
+chord_direction(chord, direction)  -- Set strum pattern
 ```
 
-To see what arguments an op takes just use `i2m(op_name)` and the error message will print them.
-Alternatively, you can just read the list of ops in the `I2M_OPS` variable.
+## Example
+
+Here's a quick example that sets up, transforms, and plays a chord:
+
+```lua
+-- Set all channels time to 1 second
+ii.i2m.time(0, 1000)
+
+-- Define chord 1 to be minor triad using reverse binary
+ii.i2m.chord_binary(1, "10010001")
+
+-- Set the strum interval on chord 1 to 120ms
+ii.i2m.chord_strum(1, 120)
+
+-- Set the play direction to repeat the bass note
+ii.i2m.chord_direction(1, 5)
+
+-- Play chord 1 on channel 0, at note 60, with velocity 100
+ii.i2m.play_chord(0, 1, 60, 100) 
+```
 
 ## Limitations
 
@@ -33,7 +68,8 @@ Current limitation include:
 - No getters
 - No default channels -- all ops that can use a channel must
 - No buffer ops
-- Incomplete chord transformations
+- No MIDI CC
+- No MIDI in
 
-None of these limitations are inherent, they are just due to a lack of time. I plan to address them all 
-soon.
+It is not clear how much of the full set of `i2c2midi` ops can be supported. The current
+implementation is already close to the size limit for user script memory.
--- i2c2midi.lua
+++ i2c2midi.lua
@@ -1,102 +1,274 @@
 --- i2c2midi
 -- Simple Crow -> i2c2midi interface. Initial idea from this post: 
 -- https://llllllll.co/t/i2c2midi-a-diy-module-that-translates-i2c-to-midi/40950/399
--- 
--- i2c2midi opcodes and argument lists from:
--- https://github.com/attowatt/i2c2midi/blob/main/firmware/i2c2midi_firmware/ops.ino 
 --
+-- Current limitations: No buffer ops, no get ops, no MIDI CC
+-- Also almost at limit of user script memory on Crow.
+-- 
 -- Author: Mark Reid <mark@reid.name>
 
--- Address of i2c2midi device on i2c bus
-I2C2MIDI_ADDR = 0x3F
-
 -- Integer to byte conversion
-local function u8 (v) return string.pack("B", v) end    -- unsigned byte
-local function s8 (v) return string.pack("b", v) end    -- signed byte
-local function u16 (v) return string.pack(">H", v) end  -- unsigned short (2 bytes)
+function u8 (v) return string.pack("B", v) end    -- unsigned byte
+function s8 (v) return string.pack("b", v) end    -- signed byte
+function u16 (v) return string.pack(">H", v) end  -- unsigned short (2 bytes)
 -- Reverse binary string to unsigned short
-local function ru16 (v) return u16(tonumber(v:reverse(), 2)) end
+function ru16 (v) return u16(tonumber(v:reverse(), 2)) end
 
--- Variables and their byte converters, and ranges
-local VARS = {
-  channel   = {u8,   {0, 32}},
-  note      = {u8,   {0, 127}},
-  note_rel  = {s8,   {-127, 127}},
-  steps     = {s8,   {-127, 127}},
-  velocity  = {u8,   {0, 127}},
-  reps      = {u8,   {1, 127}},
-  chord     = {u8,   {0, 8}},
-  length    = {u8,   {1, 8}},
-  index     = {u8,   {0, 7}},
-  rev_bin   = {ru16, nil},
-  anchor    = {u8,   {0, 16}},
-  inversion = {s8,   {-32, 32}},
-  reversed  = {u8,   {0, 127}},
-  time_ms   = {u16,  {0, 32767}},
-  curve     = {u8,   {0, 5}},
-  direction = {u8,   {0, 8}},
-  control   = {u8,   {0, 127}},
-  cc_value  = {u8,   {0, 127}},
-}
-
--- i2c2midi ops: name => {opcode, args}
-I2M_OPS = {
-  -- Settings
-  time    = {2, {"channel", "time_ms"}},
-  shift   = {4, {"channel", "note_rel"}},
-  rep     = {6, {"channel", "reps"}},
-  rat     = {8, {"channel", "reps"}},
+-- A `crow/lua/ii` style definition table
+I2M_DEF = 
+{ module_name  = 'i2c2midi'
+, manufacturer = 'attowatt'
+, i2c_address  = {0x3f}
+, lua_name     = 'i2m'
+, commands     = 
+  { { name = 'panic'
+    , cmd  = 22
+    , docs = 'Send MIDI off on all channels'
+    , args = { }
+    }
+  , { name = 'time'
+    , cmd  = 2
+    , docs = 'Set the time (ms) before note off'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'time_ms', u16, {0, 32767} }
+             }
+    }
+  , { name = 'shift'
+    , cmd  = 4
+    , docs = 'Set note shift'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'semitones', s8, {-127, 127} }
+             }
+    }
   -- Notes
-  note    = {20, {"channel", "note", "velocity"}},
-  note_o  = {21, {"channel"}},
-  panic   = {22, {}},
-  nt      = {23, {"channel", "note", "velocity", "time_ms"}},
+  , { name = 'note_reps'
+    , cmd  = 6
+    , docs = 'Set note repetition'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'repetitions', u8, {1, 127} }
+             }
+    }
+  , { name = 'note_ratchets'
+    , cmd  = 8
+    , docs = 'Set note ratcheting'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'ratchets', u8, {1, 127} }
+             }
+    }
+  , { name = 'note_on'
+    , cmd  = 20
+    , docs = 'Send note on'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'note', u8, {0, 127} }
+             , { 'velocity', u8, {1, 127} }
+             }
+    }
+  , { name = 'note_off'
+    , cmd  = 21
+    , docs = 'Send note off'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'note', u8, {0, 127} }
+             }
+    }
+  , { name = 'play_note'
+    , cmd  = 23
+    , docs = 'Send note on with duration'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'note', u8, {0, 127} }
+             , { 'velocity', u8, {1, 127} }
+             , { 'duration_ms', u16, {0, 32767} }
+             }
+    }
   -- Chords
-  chord   = {30, {"channel", "chord", "note", "velocity"}},
-  c_add   = {31, {"chord", "note_rel"}},
-  c_rm    = {32, {"chord", "note_rel"}},
-  c_clr   = {33, {"chord"}},
-  c_l     = {35, {"chord", "length"}},
-  c_inv   = {37, {"chord", "inversion"}},
-  c_rev   = {39, {"chord", "reversed"}},
-  c_rot   = {156,{"chord", "steps"}},
-  c_str   = {151,{"chord", "time_ms"}},
-  c_ins   = {152,{"chord", "index", "note_rel"}},
-  c_del   = {153,{"chord", "index"}},
-  c_set   = {154,{"chord", "index", "note_rel"}},
-  c_sc    = {158,{"chord", "chord"}},
-  c_b     = {159,{"chord", "rev_bin"}},
-  -- CC
-  cc      = {40, {"control", "cc_value"}}, 
+  , { name = 'play_chord'
+    , cmd  = 30
+    , docs = 'Play with given root'
+    , args = { { 'channel', u8, {0, 32} }
+             , { 'chord', u8, {0, 8} }
+             , { 'root', u8, {0, 127} }
+             , { 'velocity', u8, {1, 127} }
+             }
+    }
+  , { name = 'chord_add'
+    , cmd  = 31
+    , docs = 'Add a note'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'note', s8, {-127, 127} }
+             }
+    }
+  , { name = 'chord_remove'
+    , cmd  = 32
+    , docs = 'Remove a note'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'note', s8, {-127, 127} }
+             }
+    }
+  , { name = 'chord_delete'
+    , cmd  = 153
+    , docs = 'Delete indexed note'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'index', u8, {0, 7} }
+             }
+    }
+  , { name = 'chord_set'
+    , cmd  = 154
+    , docs = 'Set indexed note'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'index', u8, {0, 7} }
+             , { 'note', s8, {-127, 127} }
+             }
+    }
+  , { name = 'chord_binary'
+    , cmd  = 159
+    , docs = 'Define chord using reverse binary'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'rev_binary', ru16, nil }
+             }
+    }
+  , { name = 'chord_clear'
+    , cmd  = 33
+    , docs = 'Clear all notes'
+    , args = { { 'chord', u8, {0, 8} }
+             }
+    }
+  , { name = 'chord_length'
+    , cmd  = 35
+    , docs = 'Set length (# notes)'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'length', u8, {1, 127} }
+             }
+    }
+  , { name = 'chord_scale'
+    , cmd  = 158
+    , docs = 'Set scale of 1st chord to scale given by 2nd'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'scale_chord', u8, {0, 8} }
+             }
+    }
+  , { name = 'chord_reversal'
+    , cmd  = 39
+    , docs = 'Set reversal'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'reversed', u8, {0, 127} }
+             }
+    }
+  , { name = 'chord_rotation'
+    , cmd  = 156
+    , docs = 'Set rotation'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'steps', s8, {-127, 127} }
+             }
+    }
+  , { name = 'chord_transpose'
+    , cmd  = 160
+    , docs = 'Set transposition'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'semitones', s8, {-127, 127} }
+             }
+    }
+  , { name = 'chord_distort'
+    , cmd  = 161
+    , docs = 'Set distortion'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'width', s8, {-127, 127} }
+             , { 'anchor', u8, {0, 16} }
+             }
+    }
+  , { name = 'chord_reflect'
+    , cmd  = 162
+    , docs = 'Set reflection'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'width', s8, {-127, 127} }
+             , { 'anchor', u8, {0, 16} }
+             }
+    }
+  , { name = 'chord_inversion'
+    , cmd  = 37
+    , docs = 'Set inversion'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'inversion', s8, {-32, 32} }
+             }
+    }
+  , { name = 'chord_strum'
+    , cmd  = 151
+    , docs = 'Set duration (ms) between notes'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'time_ms', u16, {0, 32767} }
+             }
+    }
+  , { name = 'chord_vel_curve'
+    , cmd  = 163
+    , docs = 'Set velocity curve'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'type', u8, {0, 5} }
+             , { 'start_percent', u16, {0, 32767} }
+             , { 'end_percent', u16, {0, 32767} }
+             }
+    }
+  , { name = 'chord_time_curve'
+    , cmd  = 164
+    , docs = 'Set time curve'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'type', u8, {0, 5} }
+             , { 'start_percent', u16, {0, 32767} }
+             , { 'end_percent', u16, {0, 32767} }
+             }
+    }
+  , { name = 'chord_direction'
+    , cmd  = 165
+    , docs = 'Set strum pattern'
+    , args = { { 'chord', u8, {0, 8} }
+             , { 'direction', u8, {0, 8} }
+             }
+    }
+  }
 }
 
--- Convert argument value to bytes 
-local function bytes(arg, v)
-  local conv, range = table.unpack(VARS[arg])
-  -- No range check on nil range
-  if range == nil then return conv(v) end
-  -- Range check
-  local lo, hi = table.unpack(range)
-  if (v < lo) or (v > hi) then error(arg .. " value " .. v .. " not in [" .. lo .. "," .. hi .."]") end
-  -- Convert value
-  return conv(v)
+
+function init ()
+  -- Set up `ii` style hooks so that i2c2midi functions look like others
+  ii.i2m = {}
+  
+  for _, cmd in ipairs(I2M_DEF.commands) do
+    print(cmd_show(cmd))
+    ii.i2m[cmd.name] = cmd_fn(I2M_DEF.i2c_address[1], cmd)
+  end
 end
 
--- Call i2c2midi op with `op_name` and arguments
-function i2m (op_name, ...)
-  if I2M_OPS[op_name] == nil then error("Unknown op \"" .. op_name .. "\"") end
-  local vals = {...}
-  local op, args = table.unpack(I2M_OPS[op_name])
-  if #args ~= #vals then error("Usage: i2m(\"" .. op_name .. "\"," .. table.concat(args, ",") .. ")") end
-  -- Convert op + args to data
-  local tx = u8(op)
-  for i, arg in ipairs(args) do tx = tx .. bytes(arg, vals[i]) end
-  -- Send to i2c2midi
-  ii.raw(I2C2MIDI_ADDR, tx)
+-- Pretty print a command
+function cmd_show (cmd)
+  local arg_names = {}
+  for i, arg in ipairs(cmd.args) do arg_names[i] = arg[1] end
+  return cmd.name..'('..table.concat(arg_names, ', ')..')\n-- '..cmd.docs
 end
 
--- For debugging hex strings
-function string.hex(str)
-  return (str:gsub(".", function(char) return "\\x" .. string.format("%2x", char:byte()) end))
+-- Convert a command's opcode and arguments to bytes
+function cmd_bytes(cmd, vals)
+  if #cmd.args ~= #vals then error('Wrong number of args!\nUsage: '..cmd_show(cmd)) end
+  local data = u8(cmd.cmd)
+  for i, arg in ipairs(cmd.args) do
+    local v = vals[i]
+    local name, conv, range = table.unpack(arg)
+    if range == nil then 
+      -- No range check on nil range
+      data = data .. conv(v)
+    else
+      -- Range check
+      local lo, hi = table.unpack(range)
+      if (v < lo) or (v > hi) then error(name.."="..v.." not in ["..lo..","..hi.."]") end
+      data = data .. conv(v)
+    end
+  end
+  return data
 end
 
+-- Build a function from `cmd` that encodes arguments and calls `ii.raw`
+function cmd_fn (addr, cmd)
+  local function fn (...)
+    local vals = {...}
+    local data = cmd_bytes(cmd, vals)
+    ii.raw(addr, data)
+  end
+  
+  return fn
+end
